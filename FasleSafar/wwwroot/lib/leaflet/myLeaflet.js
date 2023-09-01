1a158df04c52b1cf2fcd16accf2c67bb2ab5ec8b
Mohammad Tavoosi <tavoosi.insta20@gmail.com>
2023-09-01 11:47:10 +0330

    Fix Maps

--- FasleSafar/wwwroot/lib/leaflet/myLeaflet.js
+++ FasleSafar/wwwroot/lib/leaflet/myLeaflet.js
@@ -2,7 +2,8 @@
 var geoarr = geo.split(',');
 var longitude = parseFloat(geoarr[0]);
 var latitude = parseFloat(geoarr[1]);
-var mymap = L.map('map').setView([longitude, latitude], 13);
+var mymap = L.map('map').setView([longitude, latitude], 25);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-        }).addTo(mymap);
+    }).addTo(mymap);
+L.marker([longitude, latitude]).addTo(mymap);
